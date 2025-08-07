0bc162a15192f1f99b9409a1ff7e01cf7e83c082
ClayAucoin <clay0aucoin@gmail.com>
2025-08-06 20:24:27 -0500

    added when touching star add 1 to score

--- js/sprite-interactions.js
+++ js/sprite-interactions.js
@@ -1,4 +1,14 @@
 function doSpriteInteraction() {
     // Example Code follows.
     // Please delete and replace with your own code.
+    if(player.isTouching(star1)){
+        score += 1;
+        star1.x = 0 - randomNumber(0, 10);
+        star1.y = randomNumber(10, 390);
+    }
+    if(player.isTouching(star2)){
+        score += 1;
+        star2.x = 0 - randomNumber(0, 10);
+        star2.y = randomNumber(10, 390);
+    }
 }
