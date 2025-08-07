c447b03e474e83084e50d79b5236152f79099261
ClayAucoin <clay0aucoin@gmail.com>
2025-08-06 20:17:07 -0500

    fixed movements to correct page

--- js/sprite-interactions.js
+++ js/sprite-interactions.js
@@ -1,21 +1,4 @@
 function doSpriteInteraction() {
     // Example Code follows.
     // Please delete and replace with your own code.
-    if (keyWentDown("LEFT")) {
-        player.velocityX = -2;
-        /*playSound("../sprites/boop.mp3");*/
-    }
-    if (keyWentDown("RIGHT")) {
-        player.velocityX = 2;
-    }
-    if (keyWentDown("UP")) {
-        player.velocityY = -2;
-    }
-    if (keyWentDown("DOWN")) {
-        player.velocityY = 2;
-    }
-    if (keyWentDown("SPACE")) {
-        player.velocityX = 0;
-        player.velocityY = 0;
-    }
 }
--- js/user-controls.js
+++ js/user-controls.js
@@ -1,4 +1,21 @@
 
 function respondToUser() {
+    if (keyWentDown("LEFT")) {
+        player.velocityX = -2;
+        /*playSound("../sprites/boop.mp3");*/
+    }
+    if (keyWentDown("RIGHT")) {
+        player.velocityX = 2;
+    }
+    if (keyWentDown("UP")) {
+        player.velocityY = -2;
+    }
+    if (keyWentDown("DOWN")) {
+        player.velocityY = 2;
+    }
+    if (keyWentDown("SPACE")) {
+        player.velocityX = 0;
+        player.velocityY = 0;
+    }
 
 }
