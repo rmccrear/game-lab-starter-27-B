09584ad0c6bab57d7a8b78f40a4d105f4a8ba24a
Edward Smith <213342565+EdSmith2032@users.noreply.github.com>
2025-08-06 19:54:34 -0500

    corrected file placement

--- js/user-controls.js
+++ js/user-controls.js
@@ -1,13 +1,4 @@
-// Example Code follows.
 
-// animation variables
-var alienAnimation;
-var starAnimation;
-var platformAnimation;
+function respondToUser() {
 
-// sprite variables
-var player;
-var star;
-var score = 0;
-var platform1;
-var platform2;
+}
--- js/variables.js
+++ js/variables.js
@@ -3,7 +3,11 @@
 // animation variables
 var alienAnimation;
 var starAnimation;
+var platformAnimation;
 
 // sprite variables
 var player;
 var star;
+var score = 0;
+var platform1;
+var platform2;
