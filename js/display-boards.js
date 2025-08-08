d84d954c68da0ae687c466fbee9df41ff372bee9
ClayAucoin <clay0aucoin@gmail.com>
2025-08-07 20:51:07 -0500

    changed score to red llives = 1

--- js/display-boards.js
+++ js/display-boards.js
@@ -1,10 +1,21 @@
 
 function showBoards() {
   showScore();
+  showLives();
 }
 
 function showScore() {
     fill(255, 255, 255);
     textSize(16);
     text("Score: " + score, 10, 20);
+}
+
+function showLives() {
+  if(lives > 1){
+    fill(255, 255, 255);
+  } else {
+    fill(255, 0, 0);
+  }
+  textSize(16);
+  text("Lives: " + lives, 250, 20);
 }
