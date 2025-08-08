21f797563e5cd728bccaddd19c5af95561cb3438
Edward Smith <213342565+EdSmith2032@users.noreply.github.com>
2025-08-08 18:11:21 -0500

    corrected sound

--- js/sprite-setup.js
+++ js/sprite-setup.js
@@ -9,14 +9,14 @@ function setupSprites() {
   player.scale = 1.5;
 
   // setup the star sprites
-    // star 1
+  // star 1
   star1 = createSprite(100, 100, 50, 50);
   star1.shapeColor = "aqua";
   star1.addAnimation('twinkle', starAnimation);
   star1.velocityY = 2;
   star1.height = 30;
   star1.width = 30;
-    // star 2
+  // star 2
   star2 = createSprite(100, 100, 50, 50);
   star2.shapeColor = "aqua";
   star2.addAnimation('twinkle', starAnimation);
@@ -26,13 +26,15 @@ function setupSprites() {
 
   // setup the platform sprites
   //Eddie Added
-    // platform 1
+  // platform 1
   platform1 = createSprite(randomNumber(100, 300), 325, 50, 50);
   platform1.addAnimation("platform", platformAnimation);
   platform1.velocityY = 1;
   //Eddie Added
-    // platform 2
+  // platform 2
   platform2 = createSprite(randomNumber(100, 300), 125, 50, 50);
   platform2.addAnimation("platform", platformAnimation);
-  platform2.velocityY = 1;  
+  platform2.velocityY = 1;
+
+  playSound("sprites/back-music.mp3")
 }
