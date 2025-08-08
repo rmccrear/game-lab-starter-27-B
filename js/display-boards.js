c24feecb8c78db7d332ba8a9dae78d1bd86a3934
ClayAucoin <clay0aucoin@gmail.com>
2025-08-07 21:07:24 -0500

    fixed score color on game over

--- js/display-boards.js
+++ js/display-boards.js
@@ -5,7 +5,11 @@ function showBoards() {
 }
 
 function showScore() {
+  if(lives > 0){
     fill(255, 255, 255);
+  } else {
+    fill(0, 0, 0);
+  }
     textSize(16);
     text("Score: " + score, 10, 20);
 }
@@ -14,7 +18,7 @@ function showLives() {
   if(lives > 1){
     fill(255, 255, 255);
   } else {
-    fill(255, 0, 0);
+    fill(0, 0, 0);
   }
   textSize(16);
   text("Lives: " + lives, 250, 20);
